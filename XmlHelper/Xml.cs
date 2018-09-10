db619c953221b0b67a920e9a10e46e253724d8f2
zhenghaonan <18817383615@163.com>
2018-09-10 13:30:59 +0800

    xml帮助类

---
+++ XmlHelper/Xml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Xml;
+
+namespace XmlHelper
+{
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    public class Xml
+    {
+        /// <summary>
+        /// 初始化xml文档
+        /// </summary>
+        /// <param name="filePath"></param>
+        public static void InintXmlData(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(filePath);
+                DataSet ds = new DataSet();
+                // StringReader read = new StringReader(xmlDoc.SelectSingleNode("root").SelectSingleNode("Db").OuterXml);
+                XmlNode xmlNode = xmlDoc.SelectSingleNode("root");
+                if (xmlNode == null) throw new Exception("请配置根节点root");
+                XmlNodeList nodes = xmlNode.SelectNodes("group");
+                if (nodes.Count > 0)
+                {
+                    string groupKey = string.Empty;
+                    foreach (var node in nodes)
+                    {
+                        XmlElement element = (XmlElement)node;
+                        XmlAttributeCollection collection = element.Attributes;
+                        if (collection.Count > 0)
+                        {
+                            for (int i = 0; i < collection.Count; i++)
+                            {
+
+                                if (collection[i].Name == "id")
+                                {
+                                    groupKey = collection[i].Value;
+                                    int count = element.ChildNodes.Count;
+                                    List<XmlData> xmls = new List<XmlData>();
+                                    if (count > 0)
+                                    {
+                                     
+                                        for (int j = 0; j < count; j++)
+                                        {
+                                            XmlData xml = new XmlData();
+                                            xml.Text = element.ChildNodes[j].InnerText;
+                                            XmlAttributeCollection childrenatttr = element.ChildNodes[j].Attributes;
+                                            if (childrenatttr.Count > 0)
+                                            {
+                                                
+                                                for (int k = 0; k < childrenatttr.Count; k++)
+                                                {
+                                                    if (childrenatttr[k].Name == "key")
+                                                        xml.Key = childrenatttr[k].Value;
+                                                    if (childrenatttr[k].Name == "value")
+                                                        xml.Value = childrenatttr[k].Value;
+                                                    if (childrenatttr[k].Name == "help")
+                                                        xml.Help = childrenatttr[k].Value;
+                                                    if (childrenatttr[k].Name == "description")
+                                                        xml.Description = childrenatttr[k].Value;
+                                                }
+                                                xmls.Add(xml);
+                                            }
+
+                                        }
+                                    }
+                                    if (XmlData.Data.ContainsKey(groupKey))
+                                        XmlData.Data[groupKey].AddRange(xmls);
+                                    else
+                                        XmlData.Data.Add(groupKey, xmls);
+                                }
+                            }
+                        }
+
+
+                    }
+
+                }              
+            }
+            else throw new Exception($"{filePath}不存在");
+        }
+    }
+}
---
+++ XmlHelper/XmlData.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace XmlHelper
+{
+    /// <summary>
+    /// note：1、根节点必须是root，属性可以多 但是不能少 
+    ///       2、必须有group标签分组， group 组必须要有id属性 如：<group id = "GroupTwo" ></group>
+    ///       3、group 下面的标签必须有  id key  help description 属性 如:<test  id="testOne" key="conStr"  value ="local"  help="数据库连接字符串" description="数据库连接字符串">dasdadad</test>
+    /// </summary>
+    public class XmlData
+    {
+        internal static Dictionary<string, List<XmlData>> Data = new Dictionary<string, List<XmlData>>();
+        /// <summary>
+        /// key的值
+        /// </summary>
+        public string Key { get; set; }
+        /// <summary>
+        /// id的值
+        /// </summary>
+        public String Id { get; set; }
+        /// <summary>
+        /// value 的值
+        /// </summary>
+        public String Value { get; set; }
+        /// <summary>
+        /// help 的值
+        /// </summary>
+        public String Help { get; set; }
+        /// <summary>
+        /// description的值
+        /// </summary>
+        public String Description { get; set; }
+        /// <summary>
+        /// 文本
+        /// </summary>
+        public String Text { get; set; }
+
+        /// <summary>
+        ///根据组名称获取组中的内容
+        /// </summary>
+        /// <param name="GroupKey">组的id</param>
+        /// <returns>该组所有节点的值</returns>
+        public IList<XmlData> this[String GroupKey]
+        {
+            get
+            {
+                if (Data.ContainsKey(GroupKey))
+                    return Data[GroupKey];
+                return null;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="GroupKey">组的id</param>
+        /// <param name="key">标签的key值</param>
+        /// <returns>当前标签的数据</returns>
+        public XmlData this[String GroupKey, String key]
+        {
+            get
+            {
+                if (Data.ContainsKey(GroupKey) && Data[GroupKey].Any())
+                {
+                    var nodeData = Data[GroupKey].Where(x => x.Key == key).FirstOrDefault();
+                    if (nodeData != null)
+                        return nodeData;
+                    else
+                        return null;
+                }
+                return null;
+            }
+        }
+    }
+}
